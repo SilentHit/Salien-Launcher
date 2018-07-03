ff3e5a01e28802df1c348929557fd0198495edbd
SilentHit <nekoman.hunter.tech@gmail.com>
2018-07-03 21:14:08 +1000

    Add launcher with separate config batch

---
+++ start-config.bat
@@ -0,0 +1,7 @@
+mode con: cols=120 lines=30
+powershell -command "&{$H=get-host;$W=$H.ui.rawui;$B=$W.buffersize;$B.width=120;$B.height=1000;$W.buffersize=$B;}"
+
+set IGNORE_UPDATES=0
+set DISABLE_COLORS=0
+set PREFERED_PLANET=530
+set PREFERED_ZONE=77
---
+++ start-template.bat
@@ -0,0 +1,6 @@
+@echo off
+call start-config.bat
+title Template
+
+php\php.exe -f Salien-Cheat\cheat.php
+pause
