3d69fdcaedc6ba96a81007e8e52374e4fe81a98b
SilentHit <nekoman.hunter.tech@gmail.com>
2018-07-03 21:35:45 +1000

    Fix updater

--- update-script.bat
+++ update-script.bat
@@ -4,15 +4,15 @@ echo Updating script...
 echo.
 
 set backupPath=zip\
-set scriptPath=Salien-Cheat\ Salien-Cheat2\
+set scriptPath=..\cheat\ ..\cheat2\
 
 md temp
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri https://github.com/SilentHit/Salien-Cheat/archive/master.zip -OutFile temp\SalienCheat-master.zip"
-powershell -Command "Expand-Archive -LiteralPath temp\SalienCheat-master.zip -DestinationPath temp\ -Force"
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri https://github.com/SilentHit/Salien-Cheat/archive/master.zip -OutFile temp\Salien-Cheat-master.zip"
+powershell -Command "Expand-Archive -LiteralPath temp\Salien-Cheat-master.zip -DestinationPath temp\ -Force"
 
 for %%p in (%scriptPath%) do (
 	for %%i in (%%p*) do (if not %%i == %%ptoken.txt del %%i)
-	copy temp\SalienCheat-master\* %%p >nul
+	copy temp\Salien-Cheat-master\* %%p >nul
 	echo Folder %%p updated
 )
 
@@ -20,17 +20,17 @@ if not exist "%backupPath%" (
 	md %backupPath%
 )
 
-if exist %backupPath%SalienCheat-old.zip (
-	if exist %backupPath%SalienCheat-master.zip (
-		del %backupPath%SalienCheat-old.zip
+if exist %backupPath%Salien-Cheat-old.zip (
+	if exist %backupPath%Salien-Cheat-master.zip (
+		del %backupPath%Salien-Cheat-old.zip
 	)
 )
 
-if exist %backupPath%SalienCheat-master.zip (
-	move %backupPath%SalienCheat-master.zip %backupPath%SalienCheat-old.zip >nul
+if exist %backupPath%Salien-Cheat-master.zip (
+	move %backupPath%Salien-Cheat-master.zip %backupPath%Salien-Cheat-old.zip >nul
 )
 
-move temp\SalienCheat-master.zip %backupPath% >nul
+move temp\Salien-Cheat-master.zip %backupPath% >nul
 rd /s /q temp\
 
 echo.
